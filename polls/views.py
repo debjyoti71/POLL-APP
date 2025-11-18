5eaad3e1eb6422fc330356c9d5d42dffe4cab6a9
Debjyoti Ghosh <debjyoti1ghosh@gmail.com>
2025-11-18 11:33:07 +0530

    phase 2

--- polls/views.py
+++ polls/views.py
@@ -1,9 +1,13 @@
+from django.shortcuts import render, get_object_or_404
 from .models import Poll
+
+def home(request):
+    return render(request, 'polls/home.html')
 
 def poll_list(request):
     polls = Poll.objects.all().order_by('-created_at')
     return render(request, 'polls/poll_list.html', {'polls': polls})
 
 def poll_detail(request, slug):
-    poll = Poll.objects.get(slug=slug)
+    poll = get_object_or_404(Poll, slug=slug)
     return render(request, 'polls/poll_detail.html', {'poll': poll})
